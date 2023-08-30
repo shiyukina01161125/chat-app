f943d9e579fe3724871c2c37eb09370def4f1513
shiyukina01161125 <akama.techcamp@gmail.com>
2023-08-30 15:33:33 +0900

    チャット画面の編集

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_015939) do
   end
 
   create_table "rooms", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
