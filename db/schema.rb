712019f2c1c1b73c1d9b98191cca5d744a5f20ec
shiyukina01161125 <akama.techcamp@gmail.com>
2023-08-30 10:45:06 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_112221) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_014117) do
+  create_table "rooms", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
