66fa3c56a6bde9512e3a8ef206f97d1431a4d03d
shiyukina01161125 <akama.techcamp@gmail.com>
2023-09-01 12:03:30 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
 
   def index
-     @message = Message.new
-     @room = Room.find(params[:room_id])
-     @messages = @room.messages.includes(:user)
-   end
- 
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
+end
    def create
      @room = Room.find(params[:room_id])
      @message = @room.messages.new(message_params)
