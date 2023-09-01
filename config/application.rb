77d2f74fb068b69ce2dd5678cb9000b0d960103a
shiyukina01161125 <akama.techcamp@gmail.com>
2023-09-01 15:08:53 +0900

    Active Storageの導入

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +20,11 @@ module ChatApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
