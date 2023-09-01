0da6fdd90ccfdd39e2f2c0a72b46ecfd403e4e37
shiyukina01161125 <akama.techcamp@gmail.com>
2023-09-01 14:31:27 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
 
   def index
-    @message = Message.new
-    @room = Room.find(params[:room_id])
-    @messages = @room.messages.includes(:user)
-  end
-end
+     @message = Message.new
+     @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
+   end
+ 
    def create
      @room = Room.find(params[:room_id])
      @message = @room.messages.new(message_params)
@@ -22,4 +22,5 @@ end
    def message_params
      params.require(:message).permit(:content).merge(user_id: current_user.id)
    end
+ end
  
